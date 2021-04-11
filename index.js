0a5553faea637db962547958f1a14107fdb4d335
Jordan Culver <jordanculverdev@gmail.com>
2021-04-11 17:31:11 -0500

    Add Twilio notification logic to send when NREL results greater than 0

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 var axios = require("axios").default;
 require('dotenv').config();
+const client = require('twilio')(
+    process.env.TWILIO_ACCOUNT_SID,
+    process.env.TWILIO_ACCOUNT_AUTH_TOKEN
+);
 
 var options = {
-  method: 'GET',
-  url: 'https://developer.nrel.gov/api/transportation-incentives-laws/v1.json',
-  params: {
-    api_key: process.env.NREL_API_KEY,
-    limit: '50',
-    jurisdiction: process.env.NREL_JURISDICTION,
-    incentive_type: 'GNT,TAX,LOANS,RBATE,EXEM',
-    user_type: 'IND',
-    poc: 'true',
-    recent: 'true'
-  }
+    method: 'GET',
+    url: 'https://developer.nrel.gov/api/transportation-incentives-laws/v1.json',
+    params: {
+        api_key: process.env.NREL_API_KEY,
+        limit: '50',
+        jurisdiction: process.env.NREL_JURISDICTION,
+        incentive_type: 'GNT,TAX,LOANS,RBATE,EXEM',
+        user_type: 'IND',
+        poc: 'true',
+        recent: 'true'
+    }
 };
 
 axios.request(options).then(function (response) {
-  console.log(response.data.result);
+    if (response.data.result.length === 0) return;
+    const firstResult = response.data.result[0];
+    client.messages
+        .create({
+            to: process.env.TWILIO_TO_PHONE_NUMBER,
+            messagingServiceSid: process.env.TWILIO_MESSAGING_SERVICE_SID,
+            body: `Incentive found for ${firstResult.state}: ${firstResult.plaintext}`
+        })
+        .then(message => console.log(message.sid))
+        .done();
 }).catch(function (error) {
-  console.error(error);
+    console.error(error);
 });
